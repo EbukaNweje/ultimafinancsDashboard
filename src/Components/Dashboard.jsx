9e5a7ae71146c749ef0652fee9e34a0aed5cb5c5
RaphealJames07 <123210977+RaphealJames07@users.noreply.github.com>
2023-12-24 16:30:29 +0100

    lop

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -8,8 +8,106 @@ import {PiSignOutBold} from "react-icons/pi";
 import Header from "./Header";
 import DashboardHome from "../Pages/DashboardHome";
 import Footer from "./Footer";
+import {useState} from "react";
+import Invest from "../Pages/Invest";
+import ActiveInvestment from "../Pages/ActiveInvestment";
+import Withdraw from "../Pages/Withdraw";
+import Transactions from "../Pages/Transactions";
+import Referrals from "../Pages/Referrals";
+import Security from "../Pages/Security";
+import Setting from "../Pages/Setting";
 
 const Dashboard = () => {
+    const [dashboard, setDashboard] = useState(true);
+    const [invest, setInvest] = useState(false);
+    const [activeInvestment, setActiveInvestment] = useState(false);
+    const [withdraw, setWithdraw] = useState(false);
+    const [transaction, setTransaction] = useState(false);
+    const [referrals, setReferrals] = useState(false);
+    const [security, setSecurity] = useState(false);
+    const [settings, setSettings] = useState(false);
+
+    const handleShowDashboard = () => {
+        setDashboard(true);
+        setInvest(false);
+        setActiveInvestment(false);
+        setWithdraw(false);
+        setTransaction(false);
+        setReferrals(false);
+        setSecurity(false);
+        setSettings(false);
+    };
+    const handleShowInvestment = () => {
+        setDashboard(false);
+        setInvest(true);
+        setActiveInvestment(false);
+        setWithdraw(false);
+        setTransaction(false);
+        setReferrals(false);
+        setSecurity(false);
+        setSettings(false);
+    };
+    const handleShowActiveInvestment = () => {
+        setDashboard(true);
+        setInvest(false);
+        setActiveInvestment(false);
+        setWithdraw(false);
+        setTransaction(false);
+        setReferrals(false);
+        setSecurity(false);
+        setSettings(false);
+    };
+    const handleShowWithdraw = () => {
+        setDashboard(false);
+        setInvest(false);
+        setActiveInvestment(false);
+        setWithdraw(true);
+        setTransaction(false);
+        setReferrals(false);
+        setSecurity(false);
+        setSettings(false);
+    };
+    const handleShowTransaction = () => {
+        setDashboard(false);
+        setInvest(false);
+        setActiveInvestment(false);
+        setWithdraw(false);
+        setTransaction(true);
+        setReferrals(false);
+        setSecurity(false);
+        setSettings(false);
+    };
+    const handleShowReferrals = () => {
+        setDashboard(false);
+        setInvest(false);
+        setActiveInvestment(false);
+        setWithdraw(false);
+        setTransaction(false);
+        setReferrals(true);
+        setSecurity(false);
+        setSettings(false);
+    };
+    const handleShowSecurity = () => {
+        setDashboard(false);
+        setInvest(false);
+        setActiveInvestment(false);
+        setWithdraw(false);
+        setTransaction(false);
+        setReferrals(false);
+        setSecurity(true);
+        setSettings(false);
+    };
+    const handleShowSettings = () => {
+        setDashboard(false);
+        setInvest(false);
+        setActiveInvestment(false);
+        setWithdraw(false);
+        setTransaction(false);
+        setReferrals(false);
+        setSecurity(false);
+        setSettings(true);
+    };
+
     return (
         <>
             <div className="w-full h-max flex justify-between">
@@ -27,39 +125,63 @@ const Dashboard = () => {
                     </div>
                     <div className="w-full h-[63vh] text-xs text-[rgb(128,148,174)] font-bold flex flex-col gap-4 overflow-y-auto">
                         <p>MENU</p>
-                        <div className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer">
+                        <div
+                            className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer"
+                            onClick={handleShowDashboard}
+                        >
                             <MdOutlineDashboard className="w-6 h-6" />
                             <p className="text-[15px] font-bold ">Dashboard</p>
                         </div>
-                        <div className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer">
+                        <div
+                            className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer"
+                            onClick={handleShowInvestment}
+                        >
                             <LuUserSquare2 className="w-6 h-6" />
                             <p className="text-[15px] font-bold ">Invest</p>
                         </div>
-                        <div className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer">
+                        <div
+                            className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer"
+                            onClick={handleShowActiveInvestment}
+                        >
                             <LuWallet className="w-6 h-6" />
                             <p className="text-[15px] font-bold ">
                                 Active Investment
                             </p>
                         </div>
-                        <div className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer">
+                        <div
+                            className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer"
+                            onClick={handleShowWithdraw}
+                        >
                             <IoRepeat className="w-6 h-6" />
                             <p className="text-[15px] font-bold ">Withdrawal</p>
                         </div>
-                        <div className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer">
+                        <div
+                            className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer"
+                            onClick={handleShowTransaction}
+                        >
                             <RiUserSettingsLine className="w-6 h-6" />
                             <p className="text-[15px] font-bold ">
                                 Transactions
                             </p>
                         </div>
-                        <div className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer">
+                        <div
+                            className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer"
+                            onClick={handleShowReferrals}
+                        >
                             <RiUserSettingsLine className="w-6 h-6" />
                             <p className="text-[15px] font-bold ">Referrals</p>
                         </div>
-                        <div className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer">
+                        <div
+                            className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer"
+                            onClick={handleShowSecurity}
+                        >
                             <RiUserSettingsLine className="w-6 h-6" />
                             <p className="text-[15px] font-bold ">Security</p>
                         </div>
-                        <div className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer">
+                        <div
+                            className="w-full h-10 py-2 flex items-center gap-3 text-[#526484] hover:text-[rgb(101,118,255)] hover:bg-white rounded transition-all cursor-pointer"
+                            onClick={handleShowSettings}
+                        >
                             <RiUserSettingsLine className="w-6 h-6" />
                             <p className="text-[15px] font-bold ">Settings</p>
                         </div>
@@ -70,9 +192,25 @@ const Dashboard = () => {
                     </div>
                 </div>
                 <div className="w-[78%] phone:w-full h-screen overflow-y-auto bg-black relative">
-                  <Header/>
-                  <DashboardHome/>
-                  <Footer/>
+                    <Header />
+                    {dashboard ? (
+                        <DashboardHome />
+                    ) : invest ? (
+                        <Invest />
+                    ) : activeInvestment ? (
+                        <ActiveInvestment />
+                    ) : withdraw ? (
+                        <Withdraw />
+                    ) : transaction ? (
+                        <Transactions />
+                    ) : referrals ? (
+                        <Referrals />
+                    ) : security ? (
+                        <Security />
+                    ) : settings ? (
+                        <Setting />
+                    ) : null}
+                    <Footer />
                 </div>
             </div>
         </>
