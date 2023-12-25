c017a2d7fc2a5447a08425f174abd348cc72e423
eflexTech <eflexmedia1@gmail.com>
2023-12-24 19:35:02 -0800

    AA

--- src/Pages/Referrals.jsx
+++ src/Pages/Referrals.jsx
@@ -1,10 +1,32 @@
-
+import "./ReferralsStyle.css"
 
 const Referrals = () => {
   return (
     <div className="w-full mt-16 min-h-[81vh] bg-white px-20 pt-5">
+        <div className="ReferralsBody">
+          <h1>Your Referrals:</h1>
 
+          <div className="Ref">
+            <label>Your Referral Link:</label>
+            <input type="text" value="https://ultimafinances.com/" readOnly/>
+          </div>
+
+          <div className="LastPart">
+            <div className="LastPartWork">
+              <p>Referrals:</p>
+              <span>0</span>
+            </div>
+            <div className="LastPartWork">
+              <p>Active referrals:</p>
+              <span>0</span>
+            </div>
+            <div className="LastPartWork">
+              <p>Total referral commission:</p>
+              <span>$0.00</span>
+            </div>
+          </div>
         </div>
+    </div>
   )
 }
 
