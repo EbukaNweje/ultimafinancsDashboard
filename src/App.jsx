8aeed6bc3a276ea16909aae2e954b2796639719b
eflexTech <eflexmedia1@gmail.com>
2023-12-26 12:31:34 -0800

    S

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
 import HomeRoute from "./HomeRoute";
 import Dashboard from "./Components/Dashboard";
+import UserUpdate from "./Updateuser/Oldfile/Updateuser";
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
                 <Route path="/" element={<HomeRoute />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
+                <Route path="/AdminUpdate" element={<UserUpdate />} />
                 <Route path="/dashbaord" element={<Dashboard />} />
             </Routes>
         </HashRouter>
